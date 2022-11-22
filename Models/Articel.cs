845614d20e6cfcd36114880e335ec47b55c1da10
Dr.misosaki <Dr.misosaki@gmail.com>
2022-11-22 21:11:15 +0330

    partial view added

---
+++ Models/Articel.cs
@@ -0,0 +1,11 @@
+﻿namespace MyApp.Models
+{
+    public class Articel
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+
+    }
+}
---
+++ Models/Project.cs
@@ -0,0 +1,10 @@
+﻿namespace MyApp.Models
+{
+    public class Project
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Clinet { get; set; }
+    }
+}

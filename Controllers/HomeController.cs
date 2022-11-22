f0652c233a1b88a2b2500804f810ab4f3b92d6c4
Dr.misosaki <Dr.misosaki@gmail.com>
2022-11-22 20:45:52 +0330

    templates added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,22 +6,21 @@ namespace MyApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        
+        public HomeController()
         {
-            _logger = logger;
+          
         }
 
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
+      
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
